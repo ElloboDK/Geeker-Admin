2c7fc6c13c008976368665d54e5e5940d5f4b291
Banana <1587184311@qq.com>
2024-02-29 10:14:37 +0800

    feat: 任务详情界面

--- src/views/dataquality/list/detail.vue
+++ src/views/dataquality/list/detail.vue
@@ -1,6 +1,31 @@
 <template>
-  <div class="card content-box">
-    <ECharts :option="option" />
+  <div class="detail">
+    <div class="card">
+      <ECharts :option="option" />
+    </div>
+    <div class="card">
+      <el-descriptions class="margin-top" title="分析结果" :column="1" border>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">峰宽占比</div>
+          </template>
+          70%
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">峰重叠率</div>
+          </template>
+          70%
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">峰数量</div>
+          </template>
+          10
+        </el-descriptions-item>
+      </el-descriptions>
+      <el-image :src="img" />
+    </div>
   </div>
 </template>
 
@@ -8,17 +33,18 @@
 import { useRoute } from "vue-router";
 import { ECOption } from "@/components/ECharts/config";
 import ECharts from "@/components/ECharts/index.vue";
+import img from "@/views/dataquality/list/pic1.png";
 
 const route = useRoute();
 const option: ECOption = {
   title: {
-    text: "Basic Radar Chart",
+    text: "数据质量分析",
     textStyle: {
       color: "#a1a1a1"
     }
   },
   legend: {
-    data: ["Allocated Budget", "Actual Spending"],
+    data: ["数据集"],
     textStyle: {
       color: "#a1a1a1"
     }
@@ -26,27 +52,27 @@ const option: ECOption = {
   radar: {
     // shape: 'circle',
     indicator: [
-      { name: "完整性", max: 6500 },
-      { name: "可靠性", max: 16000 },
-      { name: "准确性", max: 30000 },
-      { name: "一致性", max: 38000 }
+      { name: "完整性", max: 1 },
+      { name: "可靠性", max: 1 },
+      { name: "准确性", max: 1 },
+      { name: "一致性", max: 1 }
     ]
   },
   series: [
     {
-      name: "Budget vs spending",
+      name: "数据质量分析",
       type: "radar",
       data: [
         {
-          value: [4200, 3000, 20000, 35000],
+          value: [0.7, 0.6, 0.4, 0.8],
           name: "Allocated Budget"
-        },
-        {
-          value: [5000, 14000, 28000, 26000],
-          name: "Actual Spending"
         }
       ]
     }
   ]
 };
 </script>
+
+<style scoped lang="scss">
+@import "./index.scss";
+</style>
